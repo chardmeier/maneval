e057696838e9f30f846064096207fb63b3823c41
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2020-04-07 17:35:22 +0100

    Task ID confusion.

--- setup/prepare-evalrecords.sql
+++ setup/prepare-evalrecords.sql
@@ -189,64 +189,64 @@ transference.name='discourse/transference.txt';
 -- Create judgement records
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 1, item, corpus1, corpus2, line
+select tasks.id, item, corpus1, corpus2, line
 from tasks, general_set
 where tasks.id=1;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 1, item, corpus2, corpus3, line
+select tasks.id, item, corpus2, corpus3, line
 from tasks, general_set
 where tasks.id=1;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 1, item, corpus1, corpus3, line
+select tasks.id, item, corpus1, corpus3, line
 from tasks, general_set
 where tasks.id=1;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 2, item, corpus1, corpus2, line
+select tasks.id, item, corpus1, corpus2, line
 from tasks, general_set
 where tasks.id=2;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 2, item, corpus2, corpus3, line
+select tasks.id, item, corpus2, corpus3, line
 from tasks, general_set
 where tasks.id=2;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 2, item, corpus1, corpus3, line
+select tasks.id, item, corpus1, corpus3, line
 from tasks, general_set
 where tasks.id=2;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 3, item, corpus1, corpus2, line
+select tasks.id, item, corpus1, corpus2, line
 from tasks, discourse_set
-where tasks.id=1;
+where tasks.id=3;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 3, item, corpus2, corpus3, line
+select tasks.id, item, corpus2, corpus3, line
 from tasks, discourse_set
-where tasks.id=1;
+where tasks.id=3;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 3, item, corpus1, corpus3, line
+select tasks.id, item, corpus1, corpus3, line
 from tasks, discourse_set
-where tasks.id=1;
+where tasks.id=3;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 4, item, corpus1, corpus2, line
+select tasks.id, item, corpus1, corpus2, line
 from tasks, discourse_set
-where tasks.id=2;
+where tasks.id=4;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 4, item, corpus2, corpus3, line
+select tasks.id, item, corpus2, corpus3, line
 from tasks, discourse_set
-where tasks.id=2;
+where tasks.id=4;
 
 insert into judgments (task_id, item, corpus1, corpus2, line)
-select 4, item, corpus1, corpus3, line
+select tasks.id, item, corpus1, corpus3, line
 from tasks, discourse_set
-where tasks.id=2;
+where tasks.id=4;
 
 -- Judgements for the second annotator are the same, except for the
 -- swapping of adequacy and fluency in the task description.
